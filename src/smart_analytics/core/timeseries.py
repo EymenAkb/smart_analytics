36a730ad0abdb7f2eef75603d7da1f75c2eb41fa
EymenAkb <eyak1441@gmail.com>
2026-08-13 19:07:00 +0300

    test update (time series): remote *test* update
    This is a remote test project, it may (abd probably will) cause to inaccuracies, it is for testing purposes.

--- src/smart_analytics/core/timeseries.py
+++ src/smart_analytics/core/timeseries.py
@@ -1,28 +1,38 @@
 import pandas as pd
-import numpy as np
 import plotly.express as px
 from typing import Literal, get_args
 from smart_analytics.core.data import Data
 import io
+import warnings
 
-marginal_literal = Literal[None, "box"]
+marginal_literal = Literal[None, "box", "violin", "rug"]
 
 class SmartTimeSeries:
     def __init__(self, df: pd.DataFrame | str | io.BytesIO | None = None, date_column = None, date_format="mixed",index_column = None, 
-                 visualize_line_graph_numericak : bool = False, visualize_scatter :bool = False, visualize_area_graph:bool=False, visualize_line_categorical:bool=False,
-                 save_scatter_graphs:bool=False, save_line_graphs_numerical:bool=False, save_area_graphs:bool=False, save_line_categorical:bool=False):
+                 visualize_line_graph_numerical : bool = False, visualize_scatter :bool = False, visualize_area_graph:bool=False, visualize_line_categorical:bool=False,
+                 visualize_treemap:bool=False,
+                 save_scatter_graphs:bool=False, save_line_graphs_numerical:bool=False, save_area_graphs:bool=False, save_line_categorical:bool=False,
+                 save_treemap:bool=False,
+                 scatter_marginal_y:marginal_literal=None):
         self.df = Data.load_data(df=df, can_return_none=True, date_col=date_column, date_format=date_format)
         self.date_column = date_column
         self.date_format = date_format
         self.index_column = index_column
-        self.visualize_line_numerical = visualize_line_graph_numericak
+        self.visualize_line_numerical = visualize_line_graph_numerical
         self.visualize_scatter = visualize_scatter
         self.visualize_area = visualize_area_graph
         self.visualize_line_categorical = visualize_line_categorical
+        self.visualize_treemap = visualize_treemap
         self.save_line_graphs_numerical = save_line_graphs_numerical
         self.save_scatter_graphs = save_scatter_graphs
         self.save_area_graphs = save_area_graphs
         self.save_line_categorical = save_line_categorical
+        self.save_treemap = save_treemap
+        if not scatter_marginal_y in get_args(marginal_literal):
+            warnings.warn(f"{scatter_marginal_y} not in {get_args(marginal_literal)} falling back to None", category=UserWarning)
+            self.marginal_y = None
+        else:
+            self.marginal_y = scatter_marginal_y
 
         if isinstance(self.df, pd.DataFrame):
             self._run()
@@ -38,6 +48,8 @@ class SmartTimeSeries:
         self.area_figures_dict = {}
         self.categorical_line_figures = []
         self.categorical_line_figures_dict = {}
+        self.treemap_figures = []
+        self.treemap_figures_dict = {}
 
     def _run(self):
         self._create_savings()
@@ -60,15 +72,19 @@ class SmartTimeSeries:
                         self._save_scatter(column=column, scatter_figure=sc_fig)
 
         if self.categorical_columns:
-            if self.visualize_area:
-                area_fig = self._create_area_visualization()
-                if self.save_area_graphs:
-                    self._save_area(categorical_fig=area_fig)
+            if self.visualize_treemap:
+                treemap_fig = self._create_treemap_visualization()
+                if self.save_treemap:
+                    self._save_treemap(treemap_fig)
             for column in self.categorical_columns:
                 if self.visualize_line_categorical:
-                    line_fig = self._create_line_visualization(column=column)
+                    line_fig = self._create_line_visualization(column=column, facet_col=column, facet_col_wrap=3)
                     if self.save_line_categorical:
                         self._save_categorical_line(column=column, categorical_fig=line_fig)
+                if self.visualize_area:
+                    area_fig = self._create_area_visualization(column=column, color=column, groupnorm="percent")
+                    if self.save_area_graphs:
+                        self._save_area(column=column, categorical_fig=area_fig)
                         
     def _save_numerical_line(self, column, numerical_fig):
         self.figure_list.append(numerical_fig)
@@ -82,11 +98,11 @@ class SmartTimeSeries:
         self.scatter_figures.append(scatter_figure)
         self.scatter_figures_dict[column] = scatter_figure
 
-    def _save_area(self, categorical_fig):
+    def _save_area(self, categorical_fig, column):
+        self.figure_dict[column] = categorical_fig
+        self.area_figures_dict[column] = categorical_fig
         self.figure_list.append(categorical_fig)
-        self.figure_dict["area"] = categorical_fig
         self.area_figures.append(categorical_fig)
-        self.area_figures_dict["area"] = categorical_fig
 
     def _save_categorical_line(self, column, categorical_fig):
         self.figure_list.append(categorical_fig)
@@ -94,17 +110,29 @@ class SmartTimeSeries:
         self.categorical_line_figures.append(categorical_fig)
         self.categorical_line_figures_dict[column] = categorical_fig
 
-    def _create_line_visualization(self, column):
-        line_figure = px.line(self.df, x=self.date_column, y=column)
+    def _save_treemap(self, fig):
+        self.figure_list.append(fig)
+        self.figure_dict["treemap"] = fig
+        self.treemap_figures.append(fig)
+        self.treemap_figures_dict["treemap"] = fig
+
+    def _create_line_visualization(self, column, facet_col=None,facet_col_wrap=0):
+        line_figure = px.line(self.df, x=self.date_column, y=column, markers=True, facet_col=facet_col, facet_col_wrap=facet_col_wrap)
         return line_figure
 
-    def _create_scatter_visualization(self, column, marginal_y: marginal_literal =None):
-        scatter_figure = px.scatter(self.df, x=self.date_column, y=column, marginal_y=marginal_y)
+    def _create_scatter_visualization(self, column):
+        scatter_figure = px.scatter(self.df, x=self.date_column, y=column, marginal_y=self.marginal_y)
         return scatter_figure
 
-    def _create_area_visualization(self):
-        area_figure = px.area(self.df, x=self.date_column, y=self.categorical_columns,title=f"Distribution of all categorical values", markers=True)
+    def _create_area_visualization(self, column, groupnorm="percent"):
+        area_figure = px.area(self.df, x=self.date_column, y=self.categorical_columns,title=f"Distribution of all categorical values", markers=True,
+                                color=column, groupnorm=groupnorm)
         return area_figure
+
+    def _create_treemap_visualization(self):
+        path = [self.df[self.date_column].dt.year.astype(str)] + self.categorical_columns
+        treemap = px.treemap(data_frame=self.df, path=path)
+        return treemap
 
     def _apply_transformations(self):
         if self.index_column:
@@ -134,4 +162,5 @@ class SmartTimeSeries:
 
 if __name__ == "__main__":
     df = pd.read_csv("Py.csv")
-    ts = SmartTimeSeries(df=df, date_column="Date", visualize_line_categorical=True, save_line_categorical=True, date_format="%Y")
+    ts = SmartTimeSeries(df=df, date_column="Date", date_format="%Y", visualize_scatter=True, save_scatter_graphs=True, scatter_marginal_y="violin")
+    ts.scatter_figures[0].show()
--- src/smart_analytics/ui/time_ui.py
+++ src/smart_analytics/ui/time_ui.py
@@ -3,11 +3,14 @@ from smart_analytics.core.timeseries import SmartTimeSeries
 import streamlit as st
 from typing import Literal, get_args
 import pandas as pd
+import io
+import warnings
 
-marginal_literal = Literal[None, "box"]
+marginal_literal = Literal[None, "box", "violin", "rug"]
 
 class tsui(SmartTimeSeries):
     DATE_FORMATS = {
+        "mixed (formats with different symbols)": "mixed",
         "YYYY-MM-DD (e.g., 2026-06-06)": "%Y-%m-%d",
         "YYYY-MM-DD HH:MM:SS (e.g., 2026-06-06 14:30:00)": "%Y-%m-%d %H:%M:%S",
         "MM/DD/YYYY (US, e.g., 06/06/2026)": "%m/%d/%Y",
@@ -22,27 +25,54 @@ class tsui(SmartTimeSeries):
         "MM/DD/YYYY HH:MM AM/PM (e.g., 06/06/2026 02:30 PM)": "%m/%d/%Y %I:%M %p",
         "Custom format (Enter below)": "custom"
     }
-    def __init__(self, df: pd.DataFrame | str | None = None, date_column = None, date_format="",index_column = None, 
-                 visualize_line_graph : bool = False, visualize_scatter :bool = False, save_scatter_graphs:bool=False, save_line_graphs:bool=False):
-        self.df = Data.load_data(df=df, can_return_none=True)
+    def __init__(self, df: pd.DataFrame | str | io.BytesIO | None = None, date_column = None, date_format="mixed",index_column = None, 
+                 visualize_line_graph_numerical : bool = False, visualize_scatter :bool = False, visualize_area_graph:bool=False, visualize_line_categorical:bool=False,
+                 visualize_treemap:bool=False,
+                 save_scatter_graphs:bool=False, save_line_graphs_numerical:bool=False, save_area_graphs:bool=False, save_line_categorical:bool=False,
+                 save_treemap:bool=False,
+                 scatter_marginal_y:marginal_literal=None):
+        
+        self.df = Data.load_data(df=df, can_return_none=True, date_col=date_column, date_format=date_format)
         self.date_column = date_column
-        self.chosen_format = date_format
+        self.date_format = date_format
         self.index_column = index_column
-        self.visualize_line_graph = visualize_line_graph
+        self.visualize_line_numerical = visualize_line_graph_numerical
         self.visualize_scatter = visualize_scatter
-        self.save_line_graphs = save_line_graphs
+        self.visualize_area = visualize_area_graph
+        self.visualize_line_categorical = visualize_line_categorical
+        self.visualize_treemap = visualize_treemap
+        self.save_line_graphs_numerical = save_line_graphs_numerical
         self.save_scatter_graphs = save_scatter_graphs
+        self.save_area_graphs = save_area_graphs
+        self.save_line_categorical = save_line_categorical
+        self.save_treemap = save_treemap
+        
+        if not scatter_marginal_y in get_args(marginal_literal):
+            warnings.warn(f"{scatter_marginal_y} not in {get_args(marginal_literal)} falling back to None", category=UserWarning)
+            self.marginal_y = None
+        else:
+            self.marginal_y = scatter_marginal_y
 
         if isinstance(self.df, pd.DataFrame):
             self._run()
 
+    @st.cache_data
+    def _load_dataframe(self, data_frame):
+        return Data.load_data(df=data_frame, date_col=self.date_column, date_format=self.date_format)
+    
     def _create_savings(self):
         self.figure_list = []
         self.figure_dict = {}
-        self.line_figures = []
-        self.line_figures_dict = {}
+        self.numerical_line_figures = []
+        self.numerical_line_figures_dict = {}
         self.scatter_figures = []
         self.scatter_figures_dict = {}
+        self.area_figures = []
+        self.area_figures_dict = {}
+        self.categorical_line_figures = []
+        self.categorical_line_figures_dict = {}
+        self.treemap_figures = []
+        self.treemap_figures_dict = {}
 
     def _run(self):
         self._create_savings()
@@ -53,9 +83,17 @@ class tsui(SmartTimeSeries):
         self.numerical_columns, self.categorical_columns, self.all_columns = Data.column_assigner(data=self.df, date_column=self.date_column)
         if Data.date_check(self.df, date_column=self.date_column):
             self._create_ux_sl()
-            self._create_all_cols_sl()
+            self._create_all_numerical_cols_sl()
+            self._create_ux_al()
+            self._create_all_categorical_ux_t()
         else:
             st.title("Provide date column from sidebar to start.")
+
+    def _save_numerical_line(self, column, numerical_fig):
+        self.figure_list.append(numerical_fig)
+        self.figure_dict[column] = numerical_fig
+        self.numerical_line_figures.append(numerical_fig)
+        self.numerical_line_figures_dict[column] = numerical_fig                  
 
     def _save_scatter(self, column, scatter_figure):
         self.figure_list.append(scatter_figure)
@@ -63,17 +101,75 @@ class tsui(SmartTimeSeries):
         self.scatter_figures.append(scatter_figure)
         self.scatter_figures_dict[column] = scatter_figure
 
-    def _save_line(self, column, numerical_fig):
-        self.figure_list.append(numerical_fig)
-        self.figure_dict[column] = numerical_fig
-        self.line_figures.append(numerical_fig)
-        self.line_figures_dict[column] = numerical_fig
+    def _save_area(self, column, categorical_fig):
+        self.figure_dict[column] = categorical_fig
+        self.area_figures_dict[column] = categorical_fig
+        self.figure_list.append(categorical_fig)
+        self.area_figures.append(categorical_fig)
+
+    def _save_categorical_line(self, column, categorical_fig):
+        self.figure_list.append(categorical_fig)
+        self.figure_dict[column] = categorical_fig
+        self.categorical_line_figures.append(categorical_fig)
+        self.categorical_line_figures_dict[column] = categorical_fig
+
+    def _save_treemap(self, fig):
+        self.figure_list.append(fig)
+        self.figure_dict["treemap"] = fig
+        self.treemap_figures.append(fig)
+        self.treemap_figures_dict["treemap"] = fig
+
+    def _create_ux_al(self):
+        if not self.categorical_columns or not self.date_column or not (self.visualize_area or self.visualize_line_categorical):
+            return
+        st.title("Distribution visualizations - Categorical")
+
+        for i,column in enumerate(self.categorical_columns):
+            st.header(f"Distribution of {column}")
+            c1, c2 = st.columns([1, 1])
+            if self.visualize_line_categorical and self.visualize_area:
+                with c1:
+                    line_fig = self._create_line_visualization(column=column)
+                    st.plotly_chart(line_fig, width="stretch")
+                    if self.save_line_categorical:
+                        self._save_categorical_line(column=column, categorical_fig=line_fig)
+                with c2:
+                    area_fig = self._create_area_visualization(column)
+                    st.plotly_chart(area_fig, width="stretch")
+                    if self.save_area_graphs:
+                        self._save_area(column=column, categorical_fig=area_fig)
+
+            else:
+                if self.visualize_line_categorical:
+                    fig = self._create_line_visualization(column=column)
+                    st.plotly_chart(line_fig, width="stretch")
+                    if self.save_line_categorical:
+                        self._save_categorical_line(column=column, categorical_fig=fig)
+
+                elif self.visualize_area:
+                    fig = self._create_area_visualization(column)
+                    st.plotly_chart(area_fig, width="stretch")
+                    if self.save_area_graphs:
+                        self._save_area(column=column, categorical_fig=fig)
+                else:
+                    return
+
+                with c1:
+                    st.plotly_chart(fig)
+
+    def _create_all_categorical_ux_t(self):
+        if self.visualize_treemap:
+            st.header(f"Distrubition of all categorical columns in one graph")
+            treemap_fig = self._create_treemap_visualization()
+            st.plotly_chart(treemap_fig, width="stretch")
+            if self.save_treemap:
+                self._save_treemap(treemap_fig)
 
     def _create_ux_sl(self):
         if not self.numerical_columns or not self.date_column or not (self.visualize_scatter or self.visualize_line_graph):
             return
 
-        st.title("Distribution visualizations")
+        st.title("Distribution visualizations - Numerical")
 
         for i, column in enumerate(self.numerical_columns):
             st.header(f"Distribution of {column}")
@@ -82,10 +178,10 @@ class tsui(SmartTimeSeries):
                 with c1:
                     ln_fig = self._create_line_visualization(column=column)
                     st.plotly_chart(ln_fig, width="stretch")
-                    if self.save_line_graphs:
-                        self._save_line(column=column, numerical_fig=ln_fig)
+                    if self.save_line_graphs_numerical:
+                        self._save_numerical_line(column=column, numerical_fig=ln_fig)
                 with c2:
-                    sc_fig = self._create_scatter_visualization(column=column, marginal_y=self.scatter_marginal)
+                    sc_fig = self._create_scatter_visualization(column=column)
                     st.plotly_chart(sc_fig, width="stretch")
                     if self.save_scatter_graphs:
                         self._save_scatter(column=column, scatter_figure=sc_fig)
@@ -93,7 +189,7 @@ class tsui(SmartTimeSeries):
                 target = c1
                 if self.visualize_line_graph:
                     fig = self._create_line_visualization(column=column)
-                    if self.save_line_graphs:
+                    if self.save_line_graphs_numerical:
                         self._save_line(column=column, numerical_fig=fig)
                 elif self.visualize_scatter:
                     fig = self._create_scatter_visualization(column=column, marginal_y=self.scatter_marginal)
@@ -105,11 +201,11 @@ class tsui(SmartTimeSeries):
                 with target:
                     st.plotly_chart(fig, width="stretch")
 
-    def _create_all_cols_sl(self):
+    def _create_all_numerical_cols_sl(self):
         st.header(f"Distrubition of all columns on one graph")
 
         if self.visualize_line_graph:
-                st.plotly_chart(self._create_line_visualization(column=self.numerical_columns), width="stretch")
+            st.plotly_chart(self._create_line_visualization(column=self.numerical_columns), width="stretch")
         if self.visualize_scatter:
             st.plotly_chart(self._create_scatter_visualization(column=self.numerical_columns), width="stretch")
 
@@ -137,8 +233,6 @@ class tsui(SmartTimeSeries):
 
     def _df_initilaizer(self):
         df = st.sidebar.file_uploader("Insert your DataFrame", type="csv")
-
-
         if df:
             self.df, self.numerical_columns, self.categorical_columns, self.all_columns = Data.load_data(df, return_columns=True)
 
@@ -159,11 +253,13 @@ class tsui(SmartTimeSeries):
             self.numerical_columns, self.categorical_columns, self.all_columns = Data.column_assigner(data=self.df, date_column=self.date_column)
             if Data.date_check(data=self.df, date_column=self.date_column):
                 self._create_ux_sl()
-                self._create_all_cols_sl()
+                self._create_all_numerical_cols_sl()
+                self._create_ux_al()
+                self._create_all_categorical_ux_t()
             else:
                 st.title("Provide date column from sidebar to start.")
         else:
             st.title("Provide a DataFrame from sidebar.")
 
 if __name__ == "__main__":
-    pass
+    tsui(visualize_treemap=True, save_treemap=True)()
